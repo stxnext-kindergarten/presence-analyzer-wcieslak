bad4e497cc236a27af3c1b48fa0471428329574e
Wiktor Cieślak <wiktorcie@gmail.com>
2016-03-15 16:04:52 +0100

    [6573] Fix some users not loading

--- src/presence_analyzer/static/js/average_by_month.js
+++ src/presence_analyzer/static/js/average_by_month.js
@@ -15,27 +15,32 @@ google.load("visualization", "1", {packages:["corechart"], 'language': 'pl'});
             var selected_user = $("#user_id").val(),
                 chart_div = $('#chart_div'),
                 avatar_div = $('#avatar'),
-                avatar_img = $('#avatar img');
+                avatar_img = $('#avatar img'),
+                no_data_div = $('#no_data');
             if(selected_user) {
+                no_data_div.hide();
                 loading.show();
                 avatar_div.hide();
                 chart_div.hide();
                 $.getJSON("/api/v1/average_by_month/"+selected_user, function(result) {
-                    var data = new google.visualization.DataTable();
-                    data.addColumn('string', 'Month');
-                    data.addColumn('number', 'Average hours per month');
-                    data.addRows(result);
-                    var options = {
-                        hAxis: {title: 'Month'}
-                    };
-                    $.get("/api/v1/users/"+selected_user, function(data) {
-                        avatar_img.attr('src', data.image);
-                    });
-                    avatar_div.show();
-                    chart_div.show();
+                        var data = new google.visualization.DataTable();
+                        data.addColumn('string', 'Month');
+                        data.addColumn('number', 'Average hours per month');
+                        data.addRows(result);
+                        var options = {
+                            hAxis: {title: 'Month'}
+                        };
+                        $.get("/api/v1/users/"+selected_user, function(data) {
+                            avatar_img.attr('src', data.image);
+                        });
+                        avatar_div.show();
+                        chart_div.show();
+                        loading.hide();
+                        var chart = new google.visualization.ColumnChart(chart_div[0]);
+                        chart.draw(data, options);
+                }).fail(function() {
                     loading.hide();
-                    var chart = new google.visualization.ColumnChart(chart_div[0]);
-                    chart.draw(data, options);
+                    no_data_div.show();
                 });
             }
         });
